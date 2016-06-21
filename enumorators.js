0661da6c787c836a8eee3a40142408bfaf2dc0a1
KyleBWilson49 <KyleBWilson49@gmail.com>
2016-06-20 20:26:29 -0700

    enumartor select

--- enumorators.js
+++ enumorators.js
@@ -1,15 +1,30 @@
 'use strict'
 
-Array.prototype.each = function (callback) {
+Array.prototype.myEach = function (callback) {
   for (let i = 0; i < this.length; i++) {
     callback(this[i])
   }
   return this
 }
 
-let array = [1, 2, 3, 4]
-let callback = (el) => {
-  console.log(el)
+Array.prototype.mySelect = function (callback) {
+  let result = []
+  for (let i = 0; i < this.length; i++) {
+    let el = this[i]
+    if (callback(el)) {
+      result.push(el)
+    }
+  }
+
+  return result
 }
 
-array.each(callback)
+let array = [1, 2, 3, 4, 5]
+let callback = function (el) {
+  if (el > 3) {
+    return true
+  }
+  return false
+}
+
+console.log(array.mySelect(callback))
